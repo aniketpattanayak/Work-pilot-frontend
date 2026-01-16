adbac9f1531d8dac9be1b2126d0c0c7dd5ad806b
Aniket Pattanayak <aniketpattanayak@gmail.com>
2026-01-16 22:34:47 +0530

    0.0.1

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // CRITICAL: Ensures assets load correctly on all subdomains
+  base: '/', // Ensures assets load from the root on all subdomains
   build: {
     outDir: 'dist', // Standard output directory for Vite
-  }
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: [
+      {
+        find: './runtimeConfig',
+        replacement: './runtimeConfig.browser', // Fixes specific AWS SDK issues
+      },
+    ],
+  },
 })
